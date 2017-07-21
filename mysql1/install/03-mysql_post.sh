0b36122d8860f26ad0c9d2629b1b73b37118b3dd
dan radom <danradom@users.noreply.github.com>
2017-07-20 19:00:06 -0600

    Update 03-mysql_post.sh

--- mysql1/install/03-mysql_post.sh
+++ mysql1/install/03-mysql_post.sh
@@ -4,3 +4,5 @@
 
 mysql -e "create user 'replicator'@'172.18.0.101' identified by 'replpass';"
 mysql -e "grant replication slave, replication client on *.* to 'replicator'@'172.18.0.101';"
+
+mysqladmin flush-logs
