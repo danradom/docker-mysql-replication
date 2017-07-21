77f9fc8d02bfd773ef8d398c0660a2b0e26b0b9a
dan radom <danradom@users.noreply.github.com>
2017-07-20 18:14:13 -0600

    Update start_container.sh

--- mysql1/start_container.sh
+++ mysql1/start_container.sh
@@ -5,8 +5,8 @@ docker run --rm -it -d \
 	--net mynet \
 	--ip 172.18.0.100 \
 	-e MYSQL_ALLOW_EMPTY_PASSWORD \
-	-v /home/dr/docker/images/mysql/mysql1/my_cnf/mysql1.cnf:/etc/my.cnf \
-	-v /home/dr/docker/images/mysql/mysql1:/install \
+	-v /home/dr/docker/images/mysql-replication/mysql1/my_cnf/mysql1.cnf:/etc/my.cnf \
+	-v /home/dr/docker/images/mysql-replication/mysql1:/install \
 	--hostname mysql1 \
 	--name mysql1 \
 	mysql1
